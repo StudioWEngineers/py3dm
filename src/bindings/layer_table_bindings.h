e52a2c5b199a6375fcb3a17a93d8bfa07c0ff31a
StudioWEngineers <studio.w.engineers@gmail.com>
2025-07-21 22:02:31 +0200

    fix wrong include order in layer_table_bindings.h

--- src/bindings/layer_table_bindings.h
+++ src/bindings/layer_table_bindings.h
@@ -6,8 +6,8 @@
     All rights reserved. Use of this source code is governed by a
     BSD-style license that can be found in the LICENSE file.
 */
-#include "bindings.h"
-
 #pragma once
+
+#include "bindings.h"
 
 void LayerTableBindings(nb::module_& m);
