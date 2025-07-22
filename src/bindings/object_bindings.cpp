c5115ae5c2b03e816d64efd8eeda6b2c0b8af5a9
StudioWEngineers <studio.w.engineers@gmail.com>
2025-07-22 21:17:19 +0200

    formatting

--- src/bindings/object_bindings.cpp
+++ src/bindings/object_bindings.cpp
@@ -15,15 +15,10 @@ void ObjectBindings(nb::module_& m) {
 
         /*other methods*/
         .def("get_user_string", &ObjectUtilities::GetUserString)
-
         .def("is_corrupt", &ON_Object::IsCorrupt)
-
         .def("is_valid", &ON_Object::IsValid, nb::arg("text_log") = nullptr)
-
         .def("remove_user_string", &ObjectUtilities::RemoveUserString)
-
         .def("set_user_string", &ObjectUtilities::SetUserString)
-
         .def("user_string_count", &ON_Object::UserStringCount)
     ;
 }
