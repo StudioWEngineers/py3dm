e0e4839ba4d2a62c328d945955c848eb3076977f
StudioWEngineers <studio.w.engineers@gmail.com>
2025-07-29 11:18:25 +0200

    bump version

--- src/bindings/bindings.h
+++ src/bindings/bindings.h
@@ -12,8 +12,8 @@
 #include "../opennurbs_includes.h"
 
 #define PY3DM_VERSION_MAJOR 0
-#define PY3DM_VERSION_MINOR 0
-#define PY3DM_VERSION_PATCH 1
+#define PY3DM_VERSION_MINOR 2
+#define PY3DM_VERSION_PATCH 0
 #define PY3DM_VERSION_DEV   1 // A value > 0 indicates a development release
 
 namespace nb = nanobind;
