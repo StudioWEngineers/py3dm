3a6e24a0824de96b112686f56a10af9e22838c11
StudioWEngineers <studio.w.engineers@gmail.com>
2025-11-20 19:47:20 +0100

    add tests for Model.reset

--- tests/test_model.py
+++ tests/test_model.py
@@ -9,7 +9,7 @@ from tempfile import TemporaryDirectory
 from unittest import TestCase
 
 # third party library imports
-from py3dm import Model
+from py3dm import Layer, Model, Point3d
 
 # local library specific imports
 
@@ -148,3 +148,44 @@ class WriteAndReadModelTestSuite(TestCase):
         """Test for the `revision` property.
         """
         self.assertEqual(self.model.revision, 1)
+
+
+class ResetModelTestSuite(TestCase):
+    """Tests for the `Model` class, `reset` method.
+    """
+    def setUp(self) -> None:
+        # create a model with custom info, one layer and one geometrical object
+        self.model = Model()
+        self.model.new_revision()
+        self.model.created_by = "StudioWEngineers"
+        self.model.last_edited_by = "StudioWEngineers"
+
+        self.model.LayerTable.add(Layer())
+        self.model.LineTable.add(Point3d(0, 1, 2), Point3d(0, 1, 0))
+
+        self.model.reset()
+
+    def test_created_by(self) -> None:
+        """Check the `created_by` property after `reset`.
+        """
+        self.assertEqual(self.model.created_by, "")
+
+    def test_last_edited_by(self) -> None:
+        """Check the `last_edited_by` property after `reset`.
+        """
+        self.assertEqual(self.model.last_edited_by, "")
+
+    def test_number_of_layers(self) -> None:
+        """Check the number of `Layer`s in the model after `reset`.
+        """
+        self.assertEqual(len(list(self.model.LayerTable)), 0)
+
+    def test_number_of_objects(self) -> None:
+        """Check the number of objects in the model after `reset`.
+        """
+        self.assertEqual(len(list(self.model.ObjectTable)), 0)
+
+    def test_revision(self) -> None:
+        """Check the `revision` property after `reset`.
+        """
+        self.assertEqual(self.model.revision, 0)
