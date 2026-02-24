64961fe3bf35d517e9603c0985c13e47e83a7132
StudioWEngineers <studio.w.engineers@gmail.com>
2026-02-24 21:56:05 +0100

    clean-up conf.py + custom template for autoapi

--- docs/conf.py
+++ docs/conf.py
@@ -9,15 +9,9 @@ extensions = [
     "sphinxcontrib.moderncmakedomain",
     "sphinx.ext.intersphinx",
     "autoapi.extension",
-    "sphinx.ext.intersphinx"
 ]
 
-intersphinx_mapping = {
-    "python": ("https://docs.python.org/3", None),
-}
-
 source_suffix = {'.rst': 'restructuredtext'}
-
 master_doc = "index"
 
 html_theme = "furo"
@@ -25,13 +19,33 @@ html_title = f"{project} documentation"
 
 highlight_language = "python"
 
+exclude_patterns = [
+    ".build",
+    "release.rst",
+    "autoapi/index.rst"
+]
+
 intersphinx_mapping = {
     "python": ("https://docs.python.org/3", None),
 }
 
-exclude_patterns = [".build", "release.rst"]
-
-autoapi_dirs = ['../py3dm']
-autoapi_type = 'python'
-autoapi_ignore = ['**/__init__.py']
+# -- AutoAPI --
+autoapi_dirs = ["../py3dm"]
+autoapi_type = "python"
+autoapi_ignore = ["**/__init__.py"]
 autoapi_keep_files = False
+
+autoapi_options = [
+    "members",
+    "undoc-members",
+    "show-inheritance",
+    "show-module-summary",
+    "special-members",
+    "imported-members",
+]
+
+autoapi_own_page_level = "class"
+templates_path = ["_templates"]
+
+templates_path = ['_templates']
+autoapi_template_dir = '_templates/autoapi'
